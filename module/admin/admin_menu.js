2c81d112fbd29e067a97d6e6bf2092f09925e497
JaeHo Song <thruthesky@gmail.com>
2014-01-23 22:21:40 +0900

    fixing admin_menu.js

--- module/admin/admin_menu.js
+++ module/admin/admin_menu.js
@@ -1,11 +1,18 @@
 $(function(){
-	$('li.name').mouseover(function(){
-		$(this).addClass('selected').siblings().removeClass('selected');
+	$('li.name').mouseenter(function(){
+		$(this).addClass('selected');
 		setTimeout(function(){$('li.selected > ul.submenu').css('display','block');},300);
 	
 	});
-	$('li.name').mouseout(function(){
-		$(this).removeClass('selected')
+	$('li.name').mouseleave(function(){
+		$(this).removeClass('selected');
 		setTimeout(function(){$('li > ul.submenu').css('display','none');},300);
 	});
 });
+
+
+
+
+
+
+
