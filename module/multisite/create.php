60e612d6717db80cc5c3de7c4f1d5fddda0ca0b2
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-01-23 16:37:19 +0800

    Added multisite.css to change the input field design of create.php

--- module/multisite/create.php
+++ module/multisite/create.php
@@ -1,22 +1,21 @@
-
+	
 <h1>Create Your Site</h1>
 <?
 	if ( ! login() ) return include login_first();
 ?>
 
+<div class='create-site'>
+	<form action='?'>
+		<input type='hidden' name='module' value='multisite'>
+		<input type='hidden' name='action' value='create_submit'>
 
 
-<form action='?'>
-<input type='hidden' name='module' value='multisite'>
-<input type='hidden' name='action' value='create_submit'>
+			Domain : http://<input type='text' name='sub_domain'>.<?=etc::base_domain()?><br>
+			Site Title : <input type='text' name='title'><br>
 
 
-	Domain : http://<input type='text' name='sub_domain'>.<?=etc::base_domain()?><br>
-	Site Title : <input type='text' name='title'><br>
+		<br>
 
-
-<br>
-
-<input type='submit' value='Create Site'>
-</form>
-
+		<input type='submit' value='Create Site'>
+	</form>
+</div>
