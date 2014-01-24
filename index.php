2e9dc957ee3b2ef4f593c98749a5b3847aff201a
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-01-24 18:50:35 +0800

    'first_page.php'

--- index.php
+++ index.php
@@ -5,9 +5,7 @@ include_once(G5_PATH.'/head.php');
 if ( preg_match('/^admin_/', $action) ) include x::admin_menu();
 
 if($action==null) {
-	$urlexplode = explode(".",$_SERVER['HTTP_HOST']);
-	$current_theme = md::get('.'.$urlexplode[1]);
-	include $x_dir.'/theme/'.$current_theme['theme'].'/first_page.php';
+	include $x_dir.'/first_page.php';
 }
 else include module( $action );
 include_once(G5_PATH.'/tail.php');
