2a5fbb6ec2d49c045c5cc06a3d1b9d0c9700d8a8
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-01-24 19:06:13 +0800

    Added function to include first_page.php of the theme or the Multisite page's first_page.php if $action==null

--- index.php
+++ index.php
@@ -5,7 +5,13 @@ include_once(G5_PATH.'/head.php');
 if ( preg_match('/^admin_/', $action) ) include x::admin_menu();
 
 if($action==null) {
-	include $x_dir.'/first_page.php';
+	if(etc::base_domain()==$_SERVER['HTTP_HOST']) include $x_dir.'/first_page.php';
+	else {
+		$urlexplode = explode(".",etc::base_domain());
+		$current_theme = md::get('.'.$urlexplode[1]);
+		include $x_dir.'/theme/'.$current_theme['theme'].'/first_page.php';	
+	}
+	
 }
 else include module( $action );
 include_once(G5_PATH.'/tail.php');
