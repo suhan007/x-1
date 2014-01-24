ad679e74b49782c7b4056c102d0054d57f422b59
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-01-24 15:11:51 +0800

    JS & CSS Changes for Active Dropdown Menu

--- module/admin/admin_menu.js
+++ module/admin/admin_menu.js
@@ -1,18 +1,17 @@
 $(function(){
-	$('li.active-page').siblings().removeClass('not-active-page');
+
 	$('li.active-page').parent().css('display','block');
-	$('li.active-page').parent().parent('li').css('background','#333333').css('color','#ffffff');
-	
+	$('li.active-page').parent().parent('li').css('color','#ffffff').css('background','#333333');
+
 	$('li.name').mouseenter(function(){
 		$(this).addClass('selected');
 		$('li.selected > ul.submenu').css('display','block');
-		if($('li.active-page').parent().parent('li').mouseenter()) {}
-		else $('li.active-page').parent().css('display','hide');
+		$('li.active-page').parent().css('display','none');
 	});
 	$('li.name').mouseleave(function(){
 		$(this).removeClass('selected');
-		$('li.active-page').parent().css('display','block');
 		$('li.not-active-page').parent().css('display','none');
+		$('li.active-page').parent().css('display','block');
 	}); 
 
 });
