29a5edb70522865f8fcfd0936f72d7663dc58733
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-01-27 16:05:42 +0800

    Testing Multisite Admin Update, the Domain name and the Title

--- module/multisite/admin_update.php
+++ module/multisite/admin_update.php
@@ -1,15 +1,13 @@
 <?php
-
+	$domain_update_value = ms::update_domain("$idx");
+	$domainexplode = explode(".",$domain_update_value['domain']);
 ?>
 <div class='create-site'>
+<h1> Update for <?=$domain_update_value['domain']?>.<?=etc::base_domain()?> Site </h1>
 <form action='?'>
 	<input type='hidden' name='module' value='multisite'>
 	<input type='hidden' name='action' value='admin_update_submit'>
 	<input type='hidden' name='idx' value=<?=$idx?>>
-	<?php
-		$domain_update_value = ms::update_domain("$idx");
-		$domainexplode = explode(".",$domain_update_value['domain']);
-	?>
 	<div>Domain: http://<input type='text' name='sub_domain' value='<?=$domainexplode[0]?>'>.<?=etc::base_domain()?></div>
 	<div>Site Title: <input type='text' name='title' value='<?=$domain_update_value['title']?>'></div>
 	<input type='submit' value='Update Site'>
