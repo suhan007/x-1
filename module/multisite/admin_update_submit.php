21bcc4f1469845f3d9761fe09659b4fceed13984
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-01-27 16:54:51 +0800

    Testing Multisite Admin Update, the Domain name and the Title

--- module/multisite/admin_update_submit.php
+++ module/multisite/admin_update_submit.php
@@ -2,7 +2,7 @@
 $dom_compare = ms::update_domain("$idx");
 if((($_GET['sub_domain']==null) && ($_GET['title']==null)) || (($dom_compare['title'] == $_GET['title'] && $dom_compare['domain'] == $_GET['sub_domain']))) { jsBack('No Changes');}
 else {
-	$values = array('domain' => $_GET['sub_domain'], 'title'  => $_GET['title']);
+	$values = array('domain' => $_GET['sub_domain'].'.'.etc::base_domain(), 'title'  => $_GET['title']);
 	db::update('x_multisite_config',$values,array('idx' => $idx));
 	jsBack('Successfully Changed');
 }
