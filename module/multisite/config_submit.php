50e0100780f42a13c47124467b6f1aa7581f5cc1
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-01-29 17:31:27 +0800

    simplified ms::theme_options() code

--- module/multisite/config_submit.php
+++ module/multisite/config_submit.php
@@ -3,3 +3,8 @@
 $theme_update = $_GET;
 ms::theme_options($theme_update);
 
+if((!$_GET['theme'] == '') && ($theme_config['theme'] != $_GET['theme'])) {
+	$domain = etc::domain();
+	$theme = $_GET['theme'];
+	md::config_update();
+}
