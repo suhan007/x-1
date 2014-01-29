bc71c73f90eab63ba5fecd0b022477e5d68df2e8
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-01-29 17:39:57 +0800

    simplified ms::theme_options() code

--- module/multisite/config_submit.php
+++ module/multisite/config_submit.php
@@ -2,9 +2,10 @@
 /*update data, then return*/
 $theme_update = $_GET;
 ms::theme_options($theme_update);
-
 if((!$_GET['theme'] == '') && ($theme_config['theme'] != $_GET['theme'])) {
 	$domain = etc::domain();
 	$theme = $_GET['theme'];
+	$priority = 10;
 	md::config_update();
+	//jsGo(ms::site_url(etc::domain()).'/x/?module=multisite&action=config_submit&theme=mobile');
 }
