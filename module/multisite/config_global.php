eb91b718a03c6dbb92ef25e61ad0dec0a93dd25d
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-02-05 16:22:12 +0800

    temporary: config_global_submit file upload to data/upload/multisite folder

--- module/multisite/config_global.php
+++ module/multisite/config_global.php
@@ -4,9 +4,16 @@
 		echo "You are not admin";
 		return;
 	}
+	/* 생성된 게시판 정보를 가저온다 */
+	$qb = "bo_table LIKE '" . ms::board_id( etc::domain() ) . "%'";
 	
+	$q = "SELECT bo_table, bo_subject, bo_count_write FROM $g5[board_table] WHERE $qb";
+	
+	$rows = db::rows( $q );
+	
+	$no_of_board = count($rows);	
 ?>
-<form action='?' class='config_general' method='POST'>
+<form action='?' class='config_general' method='POST' enctype='multipart/form-data'>
 		<input type='hidden' name='module' value='multisite'>
 		<input type='hidden' name='action' value='config_global_submit'>
 <div class='config'>
@@ -20,6 +27,8 @@
 			<td>
 				<input type='text' name='title' value='<?=$extra['title']?>'>
 			</td>
+		</tr>
+		<tr>
 			<td><label>Secondary Title</label></td>
 			<td>
 				<input type='text' name='secondary_title' value='<?=$extra['secondary_title']?>'>
@@ -30,19 +39,58 @@
 			<td>
 				<input type='text' name='logo_text' value='<?=$extra['logo_text']?>'>
 			</td>
-			<td><label>Header Logo</label></td>
-			<td>
-				<input type='file' name='header_logo'>
-			</td>
 		</tr>
 		<tr>
 			<td><label>Footer Text</label></td>
-			<td colspan=3>
+			<td>
 				<input type='text' name='footer_text' value='<?=$extra['footer_text']?>'>
-			</td>		
+			</td>	
 		</tr>
 		<tr>
-			<td colspan=2><input type='submit' value='submit'></td>
+
+			<td valign='top'><label>Forums on Frontpage ( Six )</label></td>
+			<td colspan=3 class='category-front'>
+			<? for ( $i = 1; $i <= 6; $i++ ) { ?>
+			<select name="forum_no_<?=$i?>">
+					<option value=''>Forum No. <?=$i?></option>
+					<? foreach ( $rows as $row ) { ?>
+						<option value="<?=$row['bo_table']?>"><?=$row['bo_subject']?></option>
+					<? } ?>
+			</select>
+			<? } ?>
+			</td>
+		</tr>
+		<tr>
+		
+		<tr>
+			<td valign='top'>		
+				<label>Header Logo</label><br>
+				<input type='file' name='header_logo'>
+			</td>
+			<td>
+				<img src="<?=ms::url_site(etc::domain()).$extra['img_url'].$extra['header_logo']?>" width=280px height=160px>
+			</td>
+		</tr>
+		<tr>
+			<td valign='top'>		
+				<label>Banner 1</label><br>
+				<input type='file' name='banner_1'>
+			</td>
+			<td>
+				<img src="<?=ms::url_site(etc::domain()).$extra['img_url'].$extra['banner_1']?>" width=280px height=160px>
+			</td>
+		</tr>
+		<tr>
+			<td valign='top'>		
+				<label>Banner 2</label><br>
+				<input type='file' name='banner_2'>
+			</td>
+			<td>
+				<img src="<?=ms::url_site(etc::domain()).$extra['img_url'].$extra['banner_2']?>" width=280px height=160px>
+			</td>
+		</tr>
+		<tr>
+			<td colspan=4><input type='submit' value='submit'></td>
 		</tr>
 	</table>
 </div>
