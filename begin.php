94962990a1db15f05679a8dca37bd4593576727f
yonggyo <lyonggyo@gmail.com>
2014-02-07 20:01:49 +0900

    work on patch

--- begin.php
+++ begin.php
@@ -1,20 +1,5 @@
 <?php
-include_once 'class/debug.php';
-include_once 'class/etc.php';
-include_once 'class/file.php';
-include_once 'class/data.php';
-include_once 'class/html.php';
-include_once 'class/url.php';
-include_once 'class/string.php';
-include_once 'class/multidomain.php';
-include_once 'class/multisite.php';
-include_once 'class/database.php';
-
-/** so @important order of place */
-include_once 'class/gnuboard.php';
-$x_dir = g::dir() . '/x';
-$x_url = g::url() . '/x';
-include_once 'class/x.php';
+include_once 'etc/class.php';
 include_once 'etc/language/default.php';
 
 /* eo */
