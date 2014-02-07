df3a3c478b97d4ffec25bce800e4b55295927f4a
yonggyo <lyonggyo@gmail.com>
2014-02-07 21:40:02 +0800

    withcenter

--- theme/withcenter/head.php
+++ theme/withcenter/head.php
@@ -66,3 +66,10 @@
 	<a href='?page=opensource'>오픈소스</a>
 	<a href='<?=g::url()?>/bbs/board.php?bo_table='>게시판</a>
 </div></div> 
+
+<div class='layout'>
+	<table class='main-content' cellpadding=0 cellspacing=0 width='100%' border=0>
+		<tr valign='top'>
+			<td class='left' width='210'><?=outlogin('withcenter')?></td>
+			<td width='10'></td>
+			<td class='content'>
--- theme/withcenter/index.php
+++ theme/withcenter/index.php
@@ -1,13 +1,4 @@
-<div class='layout'>
-	<table class='main-content' cellpadding=0 cellspacing=0 width='100%' border=0>
-		<tr valign='top'>
-			<td class='left' width='210'><?=outlogin('withcenter')?></td>
-			<td width='10'></td>
-			<td class='content'>
+
 				<?php
 					if ( $in['page'] ) include x::theme( $in['page'] );
 				?>
-			</td>
-		</tr>
-	</table>
-</div>
