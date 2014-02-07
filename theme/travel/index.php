bb48654c54e30dcd4f568b97e14dcbd36a953162
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-02-07 16:52:28 +0800

    chat theme, partial, the only thing missing is the forum widget, and the triangle icon for selected menu

---
+++ theme/travel/index.php
@@ -0,0 +1,11 @@
+<?
+/*currently I set the height to 420px just to display 3 student comments*/
+?>
+<iframe class="iframe-data" src="https://iframe.ontue.com//?module=post&amp;post_id=rate_teacher&amp;user_id=&amp;idx=&amp;action=list"
+border="0" frameborder="0" scrolling="no" scrollbar="no" allowtransparency="true" height='100%' style="height: 420px; width: 699px"></iframe>
+
+<br>
+<div style='border: solid 1px lightgray;  height: 200px; width: 100%'>
+<br>
+FORUM WIDGET
+</div>
