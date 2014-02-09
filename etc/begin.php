31489cc2a0f8698119efbe0cd3c2b8d6844b656f
JaeHo Song <thruthesky@gmail.com>
2014-02-09 17:23:04 +0800

    adding MetaWeblogAPI function

---
+++ etc/begin.php
@@ -0,0 +1,30 @@
+<?php
+
+
+// -----------------------------------------------------------------------------
+//
+// @TODO ordered by JaeHo
+// 1. FIND A BETTER PLACE FOR THIS HOOK. 'x/begin.php' IS NOT A GOOD PLACE FOR HOOK.
+// 2. Do not let admin to input CSS or Javascript.
+// 3. Do not use anonymous function. it produces error on PHP 5.2 and below.
+//
+/** first if: display sidebar to left or right based on the multisite admin settings
+	second if: attach custom css based on the multisit admin settings
+ */
+x::hook_register('tail_begin', 'hook_multisite_tail_begin');
+
+function hook_multisite_tail_begin() {
+	$extra = ms::get_extra();
+	if($extra['theme_sidebar'] == 'left') {
+	?><style>
+		#aside {float:left;}
+		#container {border-right: 0; border-left: 1px solid #dde4e9;}
+	</style><?}
+	if($extra['css_config']) {
+	?><style><?=$extra['css_config']?></style><?}
+}
+	
+
+
+
+
